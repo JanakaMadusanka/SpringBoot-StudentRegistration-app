1f52b387219c3adc41d43f5f5892074f2c0a74a4
janaka <jnk.madusanka86@gmail.com>
2023-12-25 00:25:12 +0530

    get-data-from-database

--- student-registration-app/src/main/java/edu/icet/controller/StudentController.java
+++ student-registration-app/src/main/java/edu/icet/controller/StudentController.java
@@ -4,13 +4,11 @@ import edu.icet.dto.Student;
 import edu.icet.entity.StudentEntity;
 import edu.icet.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
+@CrossOrigin
 public class StudentController {
     @Autowired
     StudentService service;
